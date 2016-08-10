12976dcfdf8ee37ba00926f6ba38779ff41cd78b
Rosendo Galindo <rosendo_galindo@hotmail.com>
2016-08-10 10:58:35 -0500

    sql aduanet still unfinished

--- sql/databaseUser.sql
+++ sql/databaseUser.sql
@@ -1,26 +1,29 @@
 #Crear usuario local
 CREATE USER 'administrador'@'localhost' IDENTIFIED BY 'ALESAN2016sys';
-GRANT ALL PRIVILEGES ON *.* TO 'administrador'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'administrador'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 #Crear usuario local y remoto
 CREATE USER 'administrador'@'%' IDENTIFIED BY 'ALESAN2016sys';
-GRANT ALL PRIVILEGES ON *.* TO 'administrador'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'administrador'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 #Crear usuario local replica aduanet
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'aduanet';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 #Crear usuario local y remoto replica aduanet
 CREATE USER 'admin'@'%' IDENTIFIED BY 'aduanet';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 #Lista de usuarios
 SELECT * FROM mysql.`user` u;
 
 #Borrar usuarios
+DROP USER 'admin'@'localhost';
+DROP USER 'admin'@'%';
 DROP USER 'administrador'@'localhost';
+DROP USER 'administrador'@'%';
 
